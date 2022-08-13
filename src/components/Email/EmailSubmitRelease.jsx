21e5d5371c90fb61aedd89b444257ae91c6fa5fc
duongspm <tranvanduong10a22016bb@gmail.com>
2022-08-13 22:54:58 +0700

    update #12: cap nhat lai chuc nang gui email

--- src/components/Email/EmailSubmitRelease.jsx
+++ src/components/Email/EmailSubmitRelease.jsx
@@ -14,20 +14,27 @@ const EmailSubmitRelease = () => {
     const form = useRef();
     const sendEmail = (e) => {
         try{
-            e.preventDefault();
-    
-            emailjs.sendForm('service_d85pzdb', 'template_ww2y9zh', form.current, 't2_DjLlpwFn4-iPKh')
-                .then((result) => {
-                    console.log(result.text);
-                }, (error) => {
-                    console.log(error.text);
-            });
-            const data = {
-                id : `${Date.now()}`,
-                email : email,
-            }
-            saveEmail(data);
-            toast.success("Cảm ơn bạn đã đăng ký! 💕");
+            if((!email)){
+                toast.danger("Reuired fields can't be empty : Try Again 😟😥");
+            } 
+            else 
+                {
+
+                    e.preventDefault();
+            
+                    emailjs.sendForm('service_d85pzdb', 'template_ww2y9zh', form.current, 't2_DjLlpwFn4-iPKh')
+                        .then((result) => {
+                            console.log(result.text);
+                        }, (error) => {
+                            console.log(error.text);
+                    });
+                    const data = {
+                        id : `${Date.now()}`,
+                        email : email,
+                    }
+                    saveEmail(data);
+                    toast.success("Cảm ơn bạn đã đăng ký! 💕");
+                }
         }
         catch (error){
             console.log(error);
@@ -51,9 +58,7 @@ const EmailSubmitRelease = () => {
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-7 h-7 text-green-800 dark:text-green-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                         </div>
-
                             <input value={email} onChange={(e) => setEmail(e.target.value)} class="revue-form-field bg-gray-50 border border-gray-300 rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full pl-20 pr-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-1 text-sm text-gray-500" placeholder="Địa chỉ email của bạn..." type="email" name="member[email]" id="member_email" required="" />
-
                         </div>
                         <div class="revue-form-actions">
                         <input type="submit" value="ĐĂNG KÝ" class="cursor-pointer font-medium rounded-full text-sm px-5 py-2.5 text-center text-white hover:text-white border border-white hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-green-300   " name="member[subscribe]" id="member_submit" />
